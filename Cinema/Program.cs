3903baef3902df880bbd2dbf07e2fbd331b3b592
LemonadeTXT <lemonadetxt@gmail.com>
2026-01-05 16:26:54 +0300

    few changes

--- Cinema/Program.cs
+++ Cinema/Program.cs
@@ -19,12 +19,15 @@ namespace Cinema
 
             builder.Services.AddControllers();
             builder.Services.AddOpenApi();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
